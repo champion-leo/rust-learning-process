f5390878b990254b5536b7860669908c0dbd8c7d
lyusan <leo.champion@protonmail.com>
2023-04-18 18:32:22 +0200

    ray-tracing: I.3.3 use Vec3 & write_color in main

--- 00.ray-tracing-in-one-weekend/src/main.rs
+++ 00.ray-tracing-in-one-weekend/src/main.rs
@@ -1,3 +1,5 @@
+use ray_tracing_in_one_weekend::{write_color, Vec3};
+
 fn main() {
     const IMAGE_WIDTH: u32 = 256;
     const IMAGE_HEIGHT: u32 = 256;
@@ -9,14 +11,12 @@ fn main() {
     for j in (0..IMAGE_HEIGHT).rev() {
         eprintln!("Scanlines remaining : {j}");
         for i in 0..IMAGE_WIDTH {
-            let r: f64 = i as f64 / (IMAGE_WIDTH - 1) as f64;
-            let g: f64 = j as f64 / (IMAGE_HEIGHT - 1) as f64;
-            let b = 0.25;
-
-            let ir: u32 = (255.999 * r) as u32;
-            let ig: u32 = (255.999 * g) as u32;
-            let ib: u32 = (255.999 * b) as u32;
-            println!("{ir} {ig} {ib}")
+            let pixel_color = Vec3::new(
+                i as f64 / (IMAGE_WIDTH - 1) as f64,
+                j as f64 / (IMAGE_HEIGHT - 1) as f64,
+                0.25,
+            );
+            write_color(pixel_color);
         }
     }
     eprintln!("Done.");
