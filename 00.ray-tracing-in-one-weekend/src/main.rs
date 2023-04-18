446c5044e65443f0163c9052515b28aa6415e33a
lyusan <leo.champion@protonmail.com>
2023-04-18 16:42:24 +0200

    ray-tracing: I.2 output an image in PPM format

--- 00.ray-tracing-in-one-weekend/src/main.rs
+++ 00.ray-tracing-in-one-weekend/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello Ray Tracing Project");
+    const IMAGE_WIDTH: u32 = 256;
+    const IMAGE_HEIGHT: u32 = 256;
+
+    println!("P3");
+    println!("{IMAGE_WIDTH} {IMAGE_HEIGHT}");
+    println!("255");
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        eprintln!("Scanlines remaining : {j}");
+        for i in 0..IMAGE_WIDTH {
+            let r: f64 = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let g: f64 = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let b = 0.25;
+
+            let ir: u32 = (255.999 * r) as u32;
+            let ig: u32 = (255.999 * g) as u32;
+            let ib: u32 = (255.999 * b) as u32;
+            println!("{ir} {ig} {ib}")
+        }
+    }
+    eprintln!("Done.");
 }
