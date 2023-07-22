73eeed34386d1f6ab8f2621837145b151a0fabef
lyusan <leo.champion@protonmail.com>
2023-07-23 00:19:37 +0200

    ray-tracing: I.10.4 schlick approximation

--- 01.ray-tracing-in-one-weekend/src/main.rs
+++ 01.ray-tracing-in-one-weekend/src/main.rs
@@ -64,7 +64,7 @@ fn main() {
     )));
     world.add(Arc::new(Sphere::new(
         Vec3::new(-1., 0., -1.),
-        0.5,
+        -0.4,
         material_left,
     )));
     world.add(Arc::new(Sphere::new(
