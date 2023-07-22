f92e9ed8b1d0b45ffd6d3c0a294f2830dce6e256
lyusan <leo.champion@protonmail.com>
2023-07-22 15:04:18 +0200

    ray-tracing: I.9.6 fuzzy metal reflection

--- 01.ray-tracing-in-one-weekend/src/material.rs
+++ 01.ray-tracing-in-one-weekend/src/material.rs
@@ -29,18 +29,19 @@ impl Scatterable for Lambertian {
 
 pub struct Metal {
     albedo: Vec3,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(albedo: Vec3) -> Self {
-        Self { albedo }
+    pub fn new(albedo: Vec3, fuzz: f64) -> Self {
+        Self { albedo , fuzz: if fuzz < 1.0 { fuzz } else { 1.0 } }
     }
 }
 
 impl Scatterable for Metal {
     fn scatter(&self, r_in: &Ray, rec: &HitRecord, attenuation: &mut Vec3, scattered: &mut Ray) -> bool {
         let reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-        *scattered = Ray::new(rec.p, reflected);
+        *scattered = Ray::new(rec.p, reflected + self.fuzz * Vec3::random_in_unit_sphere());
         *attenuation = self.albedo;
         dot(scattered.direction(), rec.normal) > 0.0
     }
