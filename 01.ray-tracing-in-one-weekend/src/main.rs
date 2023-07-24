b19ee4380dc57bab7b6f9e334e05c48aa54089a6
lyusan <leo.champion@protonmail.com>
2023-07-24 21:26:43 +0200

    ray-tracing: I.13.1 a final render

--- 01.ray-tracing-in-one-weekend/src/main.rs
+++ 01.ray-tracing-in-one-weekend/src/main.rs
@@ -1,8 +1,8 @@
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 use ray_tracing_in_one_weekend::camera::{self, Camera};
-use ray_tracing_in_one_weekend::helper::{random, INFINITY};
+use ray_tracing_in_one_weekend::helper::{random, random_range, INFINITY};
 use ray_tracing_in_one_weekend::hittable_list::HittableList;
-use ray_tracing_in_one_weekend::material::{Dielectric, Lambertian, Metal};
+use ray_tracing_in_one_weekend::material::{self, Dielectric, Lambertian, Metal, Scatterable};
 use ray_tracing_in_one_weekend::object::{Hittable, Sphere};
 use ray_tracing_in_one_weekend::ray::Ray;
 use ray_tracing_in_one_weekend::vec3::{get_color_str, unit_vector, Vec3};
@@ -74,7 +74,6 @@ fn scene_default_camera(aspect_ratio: f64) -> (HittableList, Camera) {
     let dist_to_focus = (lookfrom - lookat).length();
     let aperture = 2.0;
 
-
     let camera = Camera::new(
         lookfrom,
         lookat,
@@ -83,6 +82,61 @@ fn scene_default_camera(aspect_ratio: f64) -> (HittableList, Camera) {
         aspect_ratio,
         aperture,
         dist_to_focus,
+    );
+    return (world, camera);
+}
+
+fn scene_random(aspect_ratio: f64) -> (HittableList, Camera) {
+    let mut world = HittableList::new();
+
+    let ground_material = Arc::new(Lambertian::new(Vec3::new(0.5, 0.5, 0.5)));
+    world.add(Arc::new(Sphere::new(
+        Vec3::new(0., -1000., 0.),
+        1000.,
+        ground_material,
+    )));
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = random();
+            let center = Vec3::new(a as f64 + 0.9 * random(), 0.2, b as f64 + 0.9 * random());
+
+            if (center - Vec3::new(4., 0.2, 0.)).length() > 0.9 {
+                let sphere_material: Arc<dyn Scatterable>;
+                if choose_mat < 0.8 {
+                    // diffuse
+                    let albedo = Vec3::random() * Vec3::random();
+                    sphere_material = Arc::new(Lambertian::new(albedo));
+                    world.add(Arc::new(Sphere::new(center, 0.2, sphere_material)));
+                } else if choose_mat < 0.95 {
+                    // metal
+                    let albedo = Vec3::random_in_range(0.5, 1.);
+                    let fuzz = random_range(0., 0.5);
+                    sphere_material = Arc::new(Metal::new(albedo, fuzz));
+                    world.add(Arc::new(Sphere::new(center, 0.2, sphere_material)));
+                } else {
+                    // glass
+                    sphere_material = Arc::new(Dielectric::new(1.5));
+                    world.add(Arc::new(Sphere::new(center, 0.2, sphere_material)));
+                }
+            }
+        }
+
+        let material1 = Arc::new(Dielectric::new(1.5));
+        world.add(Arc::new(Sphere::new(Vec3::new(0., 1., 0.), 1., material1)));
+        let material2 = Arc::new(Lambertian::new(Vec3::new(0.4, 0.2, 0.1)));
+        world.add(Arc::new(Sphere::new(Vec3::new(-4., 1., 0.), 1., material2)));
+        let material3 = Arc::new(Metal::new(Vec3::new(0.7, 0.6, 0.5), 0.));
+        world.add(Arc::new(Sphere::new(Vec3::new(4., 1., 0.), 1., material3)));
+    }
+    let camera = Camera::new(
+        Vec3::new(13., 2., 3.),
+        Vec3::new(0., 0., 0.),
+        Vec3::new(0., 1., 0.),
+        20.,
+        aspect_ratio,
+        0.1,
+        10.,
     );
     return (world, camera);
 }
@@ -127,14 +181,14 @@ fn main() {
     // TODO: remove duplicate ASPECT_RATIO here and in the camera
     const ASPECT_RATIO: f64 = 3. / 2.;
 
-    const IMAGE_WIDTH: u32 = 1200 / 4;
+    const IMAGE_WIDTH: u32 = 1200;
     const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32;
     const SAMPLES_PER_PIXEL: u32 = 500;
     const MAX_DEPHT: i32 = 50;
 
     // World
 
-    let (world, camera) = scene_default_camera(ASPECT_RATIO);
+    let (world, camera) = scene_random(ASPECT_RATIO);
 
     let world = Arc::new(world.clone());
 
